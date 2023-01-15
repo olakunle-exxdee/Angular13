dcbcde9b3d5852393deab6857e2aa4d0bfb6c369
olakunle-exxdee <saheedogunsolu2016@gmail.com>
2023-01-15 09:26:00 +0100

    init one

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,22 +3,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  // 1. Render Courses in a list
+  // 2. Select a course
+  // 3. Render a selected courses
   courses = [
     {
       id: 1,
       title: 'Angular 13 Fundamentals',
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
-      favorite: true
-    }
+      favorite: true,
+    },
+    {
+      id: 2,
+      title: 'Angular 14 Fundamentals',
+      description: 'Learn the fundamentals of Angular 14',
+      percentComplete: 27,
+      favorite: true,
+    },
+    {
+      id: 3,
+      title: 'Angular 15 Fundamentals',
+      description: 'Learn the fundamentals of Angular 15',
+      percentComplete: 28,
+      favorite: true,
+    },
   ];
 
-  constructor() { }
+  selectedCourse = null;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  selectCourse(course) {
+    this.selectedCourse = course;
   }
-
+  deleteCourse(courseId) {
+    console.log('Deleted Course', courseId);
+  }
 }
